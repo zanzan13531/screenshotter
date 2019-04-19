3b12305d235171d96d71194717a8f58f14184552
zanzan13531 <zanzan13531@gmail.com>
2019-04-19 12:14:54 -0700

    added test 2 and 3, with 3 being functional so far

---
+++ src/screenshotter/test3.java
@@ -0,0 +1,43 @@
+package screenshotter;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+ 
+import javax.imageio.ImageIO;
+ 
+/**
+ * This program demonstrates how to capture a screenshot (full screen)
+ * as an image which will be saved into a file.
+ * @author www.codejava.net
+ *
+ */
+public class test3 {
+ 
+    public static void main(String[] args) {
+        try {
+            Robot robot = new Robot();
+            String format = "jpg";
+            String fileName = "FullScreenshot." + format;
+            
+            try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+             
+            System.out.println("A full screenshot saved!");
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
